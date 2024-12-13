260b2b6d67f61941af016f81b4fb057fd5c1879e
cashorcardsir <whatnamebecause@gmail.com>
2024-12-13 01:54:53 -0800

    Update data.js

--- data.js
+++ data.js
@@ -1,74 +1,86 @@
-var imgData = {
-  1: [
-    "images/captcha/puppies/1.jpg",
-    "images/captcha/puppies/2.jpg",
-    "images/captcha/puppies/3.jpg",
-    "images/captcha/puppies/4.jpg",
-    "images/captcha/puppies/5.jpg",
-    "images/captcha/puppies/6.jpg",
-    "images/captcha/puppies/7.jpg",
-    "images/captcha/puppies/8.jpg",
-    "images/captcha/puppies/9.jpg",
-    "puppies",
-  ],
-  2: [
-    "images/captcha/rhinos/1.jpg",
-    "images/captcha/rhinos/2.jpg",
-    "images/captcha/rhinos/3.jpg",
-    "images/captcha/rhinos/4.jpg",
-    "images/captcha/rhinos/5.jpg",
-    "images/captcha/rhinos/6.jpg",
-    "images/captcha/rhinos/7.jpg",
-    "images/captcha/rhinos/8.jpg",
-    "images/captcha/rhinos/9.jpg",
-    "rhinos"
-  ],
-  3: [
-    "images/captcha/hydrants/1.jpg",
-    "images/captcha/hydrants/2.jpg",
-    "images/captcha/hydrants/3.jpg",
-    "images/captcha/hydrants/9.jpg",
-    "images/captcha/hydrants/4.jpg",
-    "images/captcha/hydrants/8.jpg",
-    "images/captcha/hydrants/5.jpg",
-    "images/captcha/hydrants/6.jpg",
-    "images/captcha/hydrants/7.jpg",
-    "fire hydrants"
-  ],
-  4: [
-    "images/captcha/trains/1.jpg",
-    "images/captcha/trains/2.jpg",
-    "images/captcha/trains/3.jpg",
-    "images/captcha/trains/9.jpg",
-    "images/captcha/trains/4.jpg",
-    "images/captcha/trains/8.jpg",
-    "images/captcha/trains/5.jpg",
-    "images/captcha/trains/6.jpg",
-    "images/captcha/trains/7.jpg",
-    "trains"
-  ],
-  5: [
-    "images/captcha/trains/5.jpg",
-    "images/captcha/trains/1.jpg",
-    "images/captcha/trains/3.jpg",
-    "images/captcha/trains/7.jpg",
-    "images/captcha/trains/9.jpg",
-    "images/captcha/trains/4.jpg",
-    "images/captcha/trains/8.jpg",
-    "images/captcha/trains/2.jpg",
-    "images/captcha/trains/6.jpg",
-    "boats"
-  ],
-  6: [
-    "images/captcha/trains/2.jpg",
-    "images/captcha/trains/6.jpg",
-    "images/captcha/trains/7.jpg",
-    "images/captcha/trains/3.jpg",
-    "images/captcha/trains/9.jpg",
-    "images/captcha/trains/4.jpg",
-    "images/captcha/trains/8.jpg",
-    "images/captcha/trains/5.jpg",
-    "images/captcha/trains/1.jpg",
-    "busses"
-  ],
-};
+var imgData = [
+  {
+    images: [
+      "images/captcha/puppies/1.jpg",
+      "images/captcha/puppies/2.jpg",
+      "images/captcha/puppies/3.jpg",
+      "images/captcha/puppies/4.jpg",
+      "images/captcha/puppies/5.jpg",
+      "images/captcha/puppies/6.jpg",
+      "images/captcha/puppies/7.jpg",
+      "images/captcha/puppies/8.jpg",
+      "images/captcha/puppies/9.jpg",
+    ],
+    name: "puppies"
+  },
+  {
+    images: [
+      "images/captcha/rhinos/1.jpg",
+      "images/captcha/rhinos/2.jpg",
+      "images/captcha/rhinos/3.jpg",
+      "images/captcha/rhinos/4.jpg",
+      "images/captcha/rhinos/5.jpg",
+      "images/captcha/rhinos/6.jpg",
+      "images/captcha/rhinos/7.jpg",
+      "images/captcha/rhinos/8.jpg",
+      "images/captcha/rhinos/9.jpg",
+    ],
+    name: "rhinos"
+  },
+  {
+    images: [
+      "images/captcha/hydrants/1.jpg",
+      "images/captcha/hydrants/2.jpg",
+      "images/captcha/hydrants/3.jpg",
+      "images/captcha/hydrants/4.jpg",
+      "images/captcha/hydrants/5.jpg",
+      "images/captcha/hydrants/6.jpg",
+      "images/captcha/hydrants/7.jpg",
+      "images/captcha/hydrants/8.jpg",
+      "images/captcha/hydrants/9.jpg",
+    ],
+    name: "fire hydrants"
+  },
+  {
+    images: [
+      "images/captcha/trains/1.jpg",
+      "images/captcha/trains/2.jpg",
+      "images/captcha/trains/3.jpg",
+      "images/captcha/trains/4.jpg",
+      "images/captcha/trains/5.jpg",
+      "images/captcha/trains/6.jpg",
+      "images/captcha/trains/7.jpg",
+      "images/captcha/trains/8.jpg",
+      "images/captcha/trains/9.jpg",
+    ],
+    name: "trains"
+  },
+  {
+    images: [
+      "images/captcha/boats/1.jpg",
+      "images/captcha/boats/2.jpg",
+      "images/captcha/boats/3.jpg",
+      "images/captcha/boats/4.jpg",
+      "images/captcha/boats/5.jpg",
+      "images/captcha/boats/6.jpg",
+      "images/captcha/boats/7.jpg",
+      "images/captcha/boats/8.jpg",
+      "images/captcha/boats/9.jpg",
+    ],
+    name: "boats"
+  },
+  {
+    images: [
+      "images/captcha/buses/1.jpg",
+      "images/captcha/buses/2.jpg",
+      "images/captcha/buses/3.jpg",
+      "images/captcha/buses/4.jpg",
+      "images/captcha/buses/5.jpg",
+      "images/captcha/buses/6.jpg",
+      "images/captcha/buses/7.jpg",
+      "images/captcha/buses/8.jpg",
+      "images/captcha/buses/9.jpg",
+    ],
+    name: "buses"
+  }
+];
